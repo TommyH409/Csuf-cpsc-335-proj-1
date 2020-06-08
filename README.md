5be8863d0ee17fb4e2cf7e6c95c7cbc01d62b3e7
TommyH409 <36341132+TommyH409@users.noreply.github.com>
2020-06-08 12:57:44 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Project-1
+Alternating disks: light-dark
+
+Group members:
+
+tommy huynh tommyh@csu.fullerton.edu
+
+juheng mo henrymo@csu.fullerton.edu
---
+++ disks.hpp
@@ -0,0 +1,207 @@
+///////////////////////////////////////////////////////////////////////////////
+// disks.hpp
+//
+// Definitions for two algorithms that each solve the
+// alternating disks problem.
+//
+// As provided, this header has four functions marked with
+// TODO comments.
+// You need to write in your own implementation of these
+// functions.
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <sstream>
+#include <string>
+#include <vector>
+
+// State of one disk, either light or dark.
+enum disk_color { DISK_LIGHT, DISK_DARK };
+
+// Data structure for the state of one row of disks.
+class disk_state {
+private:
+  std::vector<disk_color> _colors;
+
+public:
+
+  disk_state(size_t light_count)
+    : _colors(light_count * 2, DISK_LIGHT) {
+
+      assert(dark_count() > 0);
+
+      for (size_t i = 1; i < _colors.size(); i += 2) {
+        _colors[i] = DISK_DARK;
+      }
+  }
+
+  // Equality operator for unit tests.
+  bool operator== (const disk_state& rhs) const {
+    return std::equal(_colors.begin(), _colors.end(), rhs._colors.begin());
+  }
+
+  size_t total_count() const {
+    return _colors.size();
+  }
+
+  size_t dark_count() const {
+    return total_count() / 2;
+  }
+
+  size_t light_count() const {
+    return dark_count();
+  }
+
+  bool is_index(size_t i) const {
+    return (i < total_count());
+  }
+
+  disk_color get(size_t index) const {
+    assert(is_index(index));
+    return _colors[index];
+  }
+
+  void swap(size_t left_index) {
+    assert(is_index(left_index));
+    auto right_index = left_index + 1;
+    assert(is_index(right_index));
+    std::swap(_colors[left_index], _colors[right_index]);
+  }
+
+  std::string to_string() const {
+    std::stringstream ss;
+    bool first = true;
+    for (auto color : _colors) {
+      if (!first) {
+        ss << " ";
+      }
+
+      if (color == DISK_LIGHT) {
+        ss << "L";
+      } else {
+        ss << "D";
+      }
+
+      first = false;
+    }
+    return ss.str();
+  }
+
+  // Return true when this disk_state is in alternating format. That means
+  // that the first disk at index 0 is light, the second disk at index 1
+  // is dark, and so on for the entire row of disks.
+  bool is_alternating() const {
+    disk_color color = DISK_DARK;
+    for(size_t i = 0; i < _colors.size(); i++){
+      if(_colors[i] == color){
+
+    // TODO: Write code for this function, including rewriting the return
+    // statement, and then delete these comments.
+        return false;
+      }
+      if(color == DISK_DARK){
+        color = DISK_LIGHT;
+      }
+      else{
+        color = DISK_DARK;
+      }
+    }
+    return true;
+  }
+
+  // Return true when this disk_state is fully sorted, with all light disks
+  // on the left (low indices) and all dark disks on the right (high
+  // indices).
+  bool is_sorted() const {
+    // TODO: Write code for this function, including rewriting the return
+    // statement, and then delete these comments.
+   for(size_t i = 0; i < light_count(); i++){
+     if(_colors[i] == DISK_DARK){
+       return false;
+     }
+   }
+   for(size_t i = light_count(); i < dark_count(); i++){
+     if(_colors[i] == DISK_LIGHT){
+       return false;
+     }
+   }
+   return true;
+  }
+};
+
+// Data structure for the output of the alternating disks problem. That
+// includes both the final disk_state, as well as a count of the number
+// of swaps performed.
+class sorted_disks {
+private:
+  disk_state _after;
+  unsigned _swap_count;
+
+public:
+
+  sorted_disks(const disk_state& after, unsigned swap_count)
+    : _after(after), _swap_count(swap_count) { }
+
+  sorted_disks(disk_state&& after, unsigned swap_count)
+    : _after(after), _swap_count(swap_count) { }
+
+  const disk_state& after() const {
+    return _after;
+  }
+
+  unsigned swap_count() const {
+    return _swap_count;
+  }
+};
+
+// Algorithm that sorts disks using the left-to-right algorithm.
+sorted_disks sort_left_to_right(const disk_state& before) {
+  // check that the input is in alternating format
+  assert(before.is_alternating());
+  
+  int count = 0;
+  disk_state disk = before;
+  for(size_t i = 0; i < disk.light_count(); i++){//from index 0 to index n/2
+    for(size_t j = 0; j < disk.total_count() - 1; j++){//from index 0 to index n-1
+      if(disk.get(j) == DISK_DARK && disk.get(j + 1) == DISK_LIGHT){//checking if the left side is dark disk and the right side is light disk
+        disk.swap(j);
+        count++;
+      }
+    }
+  }
+
+  return sorted_disks(disk_state(disk), count);
+}
+
+// Algorithm that sorts disks using the lawnmower algorithm.
+sorted_disks sort_lawnmower(const disk_state& before) {
+  // TODO: Write code for this function, including rewriting the return
+  // statement, and then delete these comments.
+  // check that the input is in alternating format
+  
+  assert(before.is_alternating());
+  int count = 0;
+  disk_state disk = before;
+  for(size_t i = 0; i < disk.light_count(); i+=2){//from index 0 to index n/2, increment by 2
+    for(size_t j = 0; j < disk.total_count() - 1; j++){//from index 0 to index n-1
+      if(disk.get(j) == DISK_DARK && disk.get(j+1) == DISK_LIGHT){//check if the left disk is dark disk and right disk is light disk
+        disk.swap(j);
+        count++;
+      }
+    }
+    for(size_t k = disk.total_count() - 1; k > 0; k--){//from index n to index 1
+      if(disk.get(k) == DISK_LIGHT && disk.get(k-1) == DISK_DARK){//check if the left disk is light disk and the right disk is dark disk
+        disk.swap(k-1);
+        count++;
+      }
+    }
+  }
+      
+    // TODO
+  return sorted_disks(disk_state(disk), count);
+
+}
